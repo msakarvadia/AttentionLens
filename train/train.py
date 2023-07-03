d853639e93d54597a43909a9f06e775ed91d551d
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2023-07-03 19:34:26 +0000

    added functionality to load model

--- train/train.py
+++ train/train.py
@@ -1,8 +1,15 @@
 import sys
 sys.path.append('../')
 from data.get_data import get_data
+from model.get_model import get_model
+import torch
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+#TODO fix device for dataloader
 dataloader = get_data(batch_size=16)
+
+model = get_model(device=device)
 
 for data in dataloader:
     print(data['text'])
