ee54bb7b324a3dec04a08e9cb56315647f1ab4b0
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2023-08-03 15:24:48 +0000

    cleaned up some user args

--- attention_lense/train/train_pl.py
+++ attention_lense/train/train_pl.py
@@ -17,16 +17,11 @@ import argparse
 #NOTE(MS): I copied these args from a different project so they might not be relavent anymore
 #### SET UP USER ARGS
 parser = argparse.ArgumentParser()
-parser.add_argument("--local_rank", type=int)
-parser.add_argument("--device", type=int)
-parser.add_argument("--lr", default=5e-5, type=float)
-parser.add_argument("--temp", default=3, type=float)
-parser.add_argument("--alpha", default=0.5, type=float)
+parser.add_argument("--lr", default=1e-3, type=float)
 parser.add_argument("--epochs", default=3, type=int)
-parser.add_argument("--warmup_steps", default=10000, type=int)
 parser.add_argument("--batch_size", default=1, type=int)
 parser.add_argument("--num_nodes", default=1, type=int)
-parser.add_argument("--mixed_precision", default=False, type=bool, help="whether to use mixed precision for training")
+parser.add_argument("--mixed_precision", default=True, type=bool, help="whether to use mixed precision for training")
 parser.add_argument("--checkpoint_mode", default="loss", type=str, choices=["step", "loss"], help="whether to checkpoint on train loss decrease or training step number")
 parser.add_argument("--num_steps_per_checkpoint", default=200, type=int, help="number of steps after which to checkpoint (only valid for checkpoint_mode='step')")
 parser.add_argument("--checkpoint_dir", default="./checkpoint/", type=str, help="directory to store checkpoint files in")
@@ -128,7 +123,7 @@ class LightningLens(pl.LightningModule):
   '''
 
   def configure_optimizers(self):
-    optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+    optimizer = torch.optim.Adam(self.parameters(), lr=args.lr)
     return optimizer
   
   #TODO(MS): register an early stopping call back which quits training if the loss/some metric drops below a certain pont
