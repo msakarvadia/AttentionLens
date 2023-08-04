805157331ae6c017cb0204aeb8a8358f6a3db729
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2023-08-04 19:34:28 +0000

    make it easy to specify commandline arguments

--- attention_lense/experiments.sh
+++ attention_lense/experiments.sh
@@ -1,3 +1,13 @@
 #!/bin/bash 
-
-qsub simple_submit.pbs --checkpoint_dir ckpt_layer0 --layer_number 0
+qsub -v "ckpt=ckpt_0, l_num=0" simple_submit.pbs
+qsub -v "ckpt=ckpt_1, l_num=1" simple_submit.pbs
+qsub -v "ckpt=ckpt_2, l_num=2" simple_submit.pbs
+qsub -v "ckpt=ckpt_3, l_num=3" simple_submit.pbs
+qsub -v "ckpt=ckpt_4, l_num=4" simple_submit.pbs
+qsub -v "ckpt=ckpt_5, l_num=5" simple_submit.pbs
+qsub -v "ckpt=ckpt_6, l_num=6" simple_submit.pbs
+qsub -v "ckpt=ckpt_7, l_num=7" simple_submit.pbs
+qsub -v "ckpt=ckpt_8, l_num=8" simple_submit.pbs
+qsub -v "ckpt=ckpt_9, l_num=9" simple_submit.pbs
+qsub -v "ckpt=ckpt_10, l_num=10" simple_submit.pbs
+qsub -v "ckpt=ckpt_11, l_num=11" simple_submit.pbs
