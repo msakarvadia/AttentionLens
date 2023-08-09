14b7e18cf5c97e9bcafcbcb2b821de859a454764
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2023-08-09 16:52:33 +0000

    finished script to extract lenses from pytorch lighting ckpts

--- attention_lense/demos/extract_lense_from_ckpt.py
+++ attention_lense/demos/extract_lense_from_ckpt.py
@@ -1,45 +1,37 @@
-import sys
-sys.path.append('../')
-from attention_lense.model.get_model import get_model
+#import sys
+#sys.path.append('../')
 from attention_lense.model.get_model import get_model
 from attention_lense.lense.get_lense import get_lense
 from attention_lense.lense.lenseA import LenseA
-#from attention_lense.train.train_pl import LightningLens
-#import attention_lense.train as AL
-#import pytorch_lightning as pl
 import torch
-import transformer_lens.utils as utils
-import torch.nn.functional as F
 import glob
 import os
+import argparse
+
+
+#### SET UP USER ARGS
+parser = argparse.ArgumentParser()
+parser.add_argument("--ckpt_dir", default="/lus/grand/projects/SuperBERT/mansisak/attn_lens_ckpts/gpt2-small/", type=str, help="path to dir containing all latest ckpts for a model")
+parser.add_argument("--save_dir", default="/lus/grand/projects/SuperBERT/mansisak/extracted_lenses/gpt2-small/", type=str, help="path to dir where script should save all extracted lenses")
+args = parser.parse_args()
 
 #single device
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
 device="cpu"
 
-model = get_model(device=device)
-
-# do the training one layer at a time to prevent ram from running out
-hook_name = 'result'
-
-
-
 #Initalize lense with model unembed/bias matrix
+model = get_model(device=device)
 lens_param = {'unembed': model.W_U, 'bias': model.b_U, 'n_head': model.cfg.n_heads, 'd_model': model.cfg.d_model, 'd_vocab': model.cfg.d_vocab, 'lense_class': LenseA}
-attn_lens = get_lense(n_layers=1, **lens_param).to(device)
-
-name = "../train/attn_lens_layer_0" #for now we hold the lense constant
-name = "/lus/eagle/projects/datascience/mansisak/AttentionLens/attention_lense/checkpoint/attnlens-layer-9-epoch=00-step=520-train_loss=0.17.ckpt"
-name = "/lus/grand/projects/SuperBERT/mansisak/attn_lens_ckpts/gpt2-small/ckpt_0/attnlens-layer-0-epoch=00-step=590-train_loss=0.16.ckpt"
+attn_lens = get_lense(n_layers=1, **lens_param)
 
 #Iterate through all ckpts
 # accessing and printing files in directory and subdirectory
-ckpt_dir = "/lus/grand/projects/SuperBERT/mansisak/attn_lens_ckpts/gpt2-small/"
+#TODO add arg for dir to iterate through and for where to save ckpts
 
 def change_dict_key(d, old_key, new_key, default_value=None):
     d[new_key] = d.pop(old_key, default_value)
 
-def extract_and_save_lense_from_ckpt(ckpt_filepath, save_filepath="sample_lense"):
+def extract_and_save_lense_from_ckpt(ckpt_filepath, save_filepath):
+
     attn_lens_cls = torch.load(ckpt_filepath, map_location='cpu')
     a = attn_lens_cls["state_dict"]
     for key in list(a.keys()):
@@ -52,11 +44,11 @@ def extract_and_save_lense_from_ckpt(ckpt_filepath, save_filepath="sample_lense"
     torch.save(attn_lens, save_filepath)
 
 
-def iter_thru_ckpts_extract_lenses(ckpt_dir, save_dir="gpt2_small"):
+def iter_thru_ckpts_extract_lenses(ckpt_dir, save_dir="gpt2_small/"):
     for filename in glob.glob(ckpt_dir+'**/*.ckpt', recursive=True):
         #print(filename)  # print file name
-        print(save_dir+"/"+os.path.basename(filename))  # print file name
-        save_filepath = save_dir+"/"+os.path.basename(filename)
+        print(save_dir+os.path.basename(filename))  # print file name
+        save_filepath = save_dir+os.path.basename(filename)
 
         #if save_dir doesn't exist, create it
         # Check whether the specified path exists or not
@@ -67,33 +59,4 @@ def iter_thru_ckpts_extract_lenses(ckpt_dir, save_dir="gpt2_small"):
 
         extract_and_save_lense_from_ckpt(filename, save_filepath=save_filepath)
 
-iter_thru_ckpts_extract_lenses(ckpt_dir)
-
-extract_and_save_lense_from_ckpt(ckpt_filepath=name, save_filepath="sample_lense")
-
-attn_lense = torch.load("sample_lense")
-
-#TODO Update layer:
-n_layer = 0
-hook_id = utils.get_act_name(hook_name, n_layer)
-
-prompt = "George Washington fought in the"
-print("Prompt: ", prompt)
-tokens = model.to_tokens(prompt)
-
-with torch.no_grad():
-    logits, cache = model.run_with_cache(tokens, remove_batch_dim=False)
-    
-inputs = []
-inputs.append(cache[hook_id])
-input_tensor = torch.stack(inputs)
-
-for head in range(12):
-    #head = 8
-    print("Head: ", head)
-    layer_0_head = attn_lens.lenses[0].linears[head]
-    projected = layer_0_head(inputs[0][0][-1][head])
-
-    topk_token_vals, topk_token_preds = torch.topk(projected, 70)
-    print(model.to_string(topk_token_preds))
-    print("______________________")
+iter_thru_ckpts_extract_lenses(args.ckpt_dir, args.save_dir)
