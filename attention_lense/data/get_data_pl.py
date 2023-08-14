007c3c27bb3efc97d13a980dbdabe34f6da8c035
Nathaniel Hudson <nchudson88@gmail.com>
2023-08-14 13:12:26 -0500

    Fix running on macOS
    Run `train_pl.py` from the root project directory

--- attention_lense/data/get_data_pl.py
+++ attention_lense/data/get_data_pl.py
@@ -3,15 +3,18 @@ from datasets import load_dataset
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 
+
 class DataModule(pl.LightningDataModule):
+    def setup(self, stage):
+        self.data = load_dataset("bookcorpus", split="train")
 
-  def setup(self, stage):
-    self.data  = load_dataset('bookcorpus', split="train")  
+    def train_dataloader(self):
+        return DataLoader(
+            self.data, batch_size=4, pin_memory=True, num_workers=0
+        )  # 16)
 
-  def train_dataloader(self):
-    return DataLoader(self.data, batch_size=4, pin_memory=True, num_workers=16)
 
-'''
+"""
 def get_data(device, streaming=True, pin_memory=True, batch_size=32, dataset_name="c4", num_workers=16):
     # will start loading the data when iterated over
     #data  = load_dataset(dataset_name, split="train", streaming=streaming)  
@@ -23,4 +26,4 @@ def get_data(device, streaming=True, pin_memory=True, batch_size=32, dataset_nam
                              pin_memory=pin_memory, pin_memory_device=device, num_workers=num_workers)
 
     return dataloader
-'''
+"""
