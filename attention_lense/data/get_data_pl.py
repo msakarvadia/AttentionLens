c2145a950cc028849b1f18cc4c0e0686d3105576
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2023-10-12 17:00:29 -0500

    updated doc strings for functions:

--- attention_lense/data/get_data_pl.py
+++ attention_lense/data/get_data_pl.py
@@ -4,6 +4,11 @@ from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 
 class DataModule(pl.LightningDataModule):
+  """Initializes a DataLoader object for "bookcorpus". Support for more datasets coming soon.
+
+  Returns:
+      A dataloader.
+  """
 
   def setup(self, stage):
     self.data  = load_dataset('bookcorpus', split="train")  
@@ -11,16 +16,3 @@ class DataModule(pl.LightningDataModule):
   def train_dataloader(self):
     return DataLoader(self.data, batch_size=4, pin_memory=True, num_workers=16)
 
-'''
-def get_data(device, streaming=True, pin_memory=True, batch_size=32, dataset_name="c4", num_workers=16):
-    # will start loading the data when iterated over
-    #data  = load_dataset(dataset_name, split="train", streaming=streaming)  
-
-    #TODO (MS): discuss with team about whether or not to preprocess batches to ensure all items have same length
-    data  = load_dataset(dataset_name, 'en', split="train", streaming=streaming)  
-    dataloader = DataLoader(data,
-                            batch_size=batch_size,
-                             pin_memory=pin_memory, pin_memory_device=device, num_workers=num_workers)
-
-    return dataloader
-'''
