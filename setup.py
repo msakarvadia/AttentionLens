070cf2af0301021f442086ee9426652a8e7bab8e
Yadu Nand Babuji <yadudoc1729@gmail.com>
2024-02-29 19:06:27 -0600

    Update setup.py to use requirements.txt
    Setup.py to use requirements.txt via install_requires

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name="attention_lens", version="0.0.1", packages=find_packages())
+with open('requirements.txt') as f:
+    install_requires = f.readlines()
+
+setup(
+  name="attention_lens", 
+  version="0.0.1",
+  install_requires=install_requires,
+  packages=find_packages())
